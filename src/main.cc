d43cf460bb76c5f48f78c7fc7f24e53d2ef41560
Bruce Gliff <ilyaawaA@yandex.ru>
2020-10-09 19:32:00 +0300

    GLAD has been added
    Now it can draw window with green fill
    Tested on Windows

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,7 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -19,11 +22,23 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* Initialize glad */
+    if (!gladLoadGL())
+    {
+        std::cerr << "Cannot initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    /* To make sure what GL has initialized */
+    std::cout << "OpenGL ver: " << GLVersion.major << " <> " << GLVersion.minor << std::endl;
+
+    glClearColor(0.f, 1.f, 0.f, 1.f);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
