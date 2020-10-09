1183054394484d86fb1aba15e0a19befb2aeaf8d
Bruce Gliff <ilyaawaA@yandex.ru>
2020-10-09 23:14:26 +0300

    Initialization of glfw and OpenGL
    Now it is react on the window's size changing and on ESCAPE button

--- src/main.cc
+++ src/main.cc
@@ -3,18 +3,48 @@
 
 #include <iostream>
 
+int g_sizeX = 640;
+int g_sizeY = 480;
+
+
+/// Functions what overrides behavior
+// Override size changes
+void glfwWindowSizeCallback(GLFWwindow* window, int width, int height)
+{
+    g_sizeX = width;
+    g_sizeY = height;
+    glViewport(0, 0, g_sizeX, g_sizeY);
+}
+// Override keyboard events
+void glfwKeyCallback(GLFWwindow* window, int key, int scanmode, int action, int mode)
+{
+    // Close window due to escape
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        // Set up window property what is has to be closed
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
 int main(void)
 {
-    GLFWwindow* window;
-
     /* Initialize the library */
     if (!glfwInit())
+    {
+        std::cerr << "glfl initialization failed!" << std::endl;
         return -1;
+    }
+        
+    /* Setup version of OpenGL. If not compared then terminated */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "gEngine", nullptr, nullptr);
     if (!window)
     {
+        std::cerr << "glfl window creating failed!" << std::endl;
         glfwTerminate();
         return -1;
     }
@@ -22,15 +52,21 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    /* Set up my own function */
+    glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+    glfwSetKeyCallback(window, glfwKeyCallback);
+
     /* Initialize glad */
     if (!gladLoadGL())
     {
         std::cerr << "Cannot initialize GLAD" << std::endl;
+        glfwTerminate();
         return -1;
     }
 
-    /* To make sure what GL has initialized */
-    std::cout << "OpenGL ver: " << GLVersion.major << " <> " << GLVersion.minor << std::endl;
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL ver: " << glGetString(GL_VERSION) << std::endl;
+
 
     glClearColor(0.f, 1.f, 0.f, 1.f);
 
