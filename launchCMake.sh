466f571904d72c383e1409608d037634bb45f6d1
Bruce Gliff <ilyaawaA@yandex.ru>
2020-10-10 19:02:10 +0300

    CMake changes
    Now it copies res/ directory to the binary directory

--- launchCMake.sh
+++ launchCMake.sh
@@ -1,11 +1,11 @@
 # Goto binary directory
-mkdir bin
-cd bin
+mkdir build
+cd build
 
-#Generate solution for different OS'es. <..> because CMakeLists.txt in project/ but project will be created in project/bin/
+#Generate solution for different OS'es. <..> because CMakeLists.txt in project/ but project will be created in project/build/
 cmake ..
 
-#Build solution. <.> becuse project in project/bin/
+#Build solution. <.> becuse project in project/build/
 cmake --build . --config Release
 
 cd ..
