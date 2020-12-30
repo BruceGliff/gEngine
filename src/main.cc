9f6f746401fe537a9e4706468ab5743e431e3b11
Bruce Gliff <ilyaawaA@yandex.ru>
2020-12-30 19:32:04 +0500

    3D objects for test added

--- src/main.cc
+++ src/main.cc
@@ -51,7 +51,7 @@ int main(int argc, char * argv[])
         // TODO Investigate is it should be there
         //pObjShaderProgram->Use();
 
-        Model::Model ourModel(resMng.getPathToExucutable() + "\\res\\models\\backpack\\backpack.obj");
+        Model::Model ourModel(resMng.getPathToExucutable() + "\\res\\models\\text\\text.obj");
 
         // Easiest benchmark
         int delta_frame = 0;
@@ -64,7 +64,7 @@ int main(int argc, char * argv[])
             ++delta_frame;
 
             /* Render here */
-            glClearColor(0.1f, 0.5f, 0.1f, 1.0f);
+            glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
             pObjShaderProgram->Use();
@@ -85,7 +85,7 @@ int main(int argc, char * argv[])
 
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
-            model = glm::scale(model, glm::vec3(1.f, 1.f, 1.f));	// it's a bit too big for our scene, so scale it down
+            model = glm::scale(model, glm::vec3(0.05f, 0.05f, 0.05f));	// it's a bit too big for our scene, so scale it down
             pObjShaderProgram->setMat4("model", model);
             ourModel.Draw(*pObjShaderProgram);
 
